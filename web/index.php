d93a97e805c30f0e6e805a0d28183f0a20537895
Kevin <rocketlikeabass@gmail.com>
2018-05-01 21:49:01 -0600

    updated index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
 
-  phpinfo();
+  <title>Kevin's Homepage</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
 
-?>
+  <link rel="stylesheet" href="css/homepage.css">
+</head>
+<body>
+    Hello again
+</body>
+</html>
