fc17a363bd323e6a5e0a38a21422268f1708f7c7
Kevin <rocketlikeabass@gmail.com>
2018-05-01 22:26:23 -0600

    homepage start

--- web/index.php
+++ web/index.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
 
-  <title>Kevin's Homepage</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
+    <title>Kevin's Homepage</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
 
-  <link rel="stylesheet" href="css/homepage.css">
+    <link rel="stylesheet" href="css/homepage.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 </head>
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div>
+                    <h1>Kevin's Homepage</h1>
+                    <h3>Information Here Maybe</h3>
+                    <hr>
+                    <button class="btn btn-default btn-lg">Click Here For Assignments</button>
+                </div>
+            </div>
+        </div>
+    </div>
     Hello again
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous">
+    </script>
+    <script type="text/javascript"  src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js">
+    </script>
 </body>
 </html>
