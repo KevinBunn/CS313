e18ba7dd746665ab20caa50a6902001ab4c5df53
Kevin Bunn <rocketlikeabass@gmail.com>
2018-05-10 16:05:05 -0600

    session inclass

--- web/prove03/viewcart.php
+++ web/prove03/viewcart.php
@@ -1,14 +1,14 @@
 <?php
   session_start();
   if (isset($_SESSION["count"])
-      $_SESSION['count'] += 1;
+      $_SESSION["count"] += 1;
   else 
-      $_SESSION['count'] = 0;
+      $_SESSION["count"] = 0;
 ?>
 <html>
   <body>
     <?php
-      echo "<h1>" . $_SESSION['count'] . "</h1>";
+      echo "<h1>" . $_SESSION["count"] . "</h1>";
       //echo "<p>viewcart</p>"
     ?>
   </body>
