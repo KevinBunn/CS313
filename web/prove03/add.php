49cbcb281b63948889b35e84c6809a1efd66e571
Kevin <rocketlikeabass@gmail.com>
2018-05-11 14:57:41 -0600

    mineral count

--- web/prove03/add.php
+++ web/prove03/add.php
@@ -2,7 +2,6 @@
     session_start();
     
     if($_POST["DarkTemplar"] != '') {
-      $_SESSION["TotalMinerals"] -= 150;
       $_SESSION["DarkTemplar"] = $_POST["DarkTemplar"];
     }
     if($_POST["Stalker"] != '') {
@@ -11,9 +10,11 @@
     if($_POST["Colossus"] != '') {
       $_SESSION["Colossus"] = $_POST["Colossus"];
     }
-    if($_POST["Pheonix"] != '') {
+    if($_POST["Pheonix"] != '') {    
       $_SESSION["Pheonix"] = $_POST["Pheonix"];
     }
     //$_SESSION["Artanis"] = $_POST["Artanis"];
     
+    $computed_cost = (2000 - (100 * $_SESSION["DarkTemplar"]) - (150 * $_SESSION["Stalker"]) - (300 * $_SESSION["Colossus"]) - (100 * $_SESSION["Pheonix"]) - (500 * $_SESSION["Artanis"]));
+    $_SESSION["TotalMinerals"] = $computed_cost;
 ?>
