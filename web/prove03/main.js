3448ed44d7e23f6695dd1ddcb82e99417661964a
Kevin Bunn <rocketlikeabass@gmail.com>
2018-05-10 23:40:46 -0600

    ajax

--- web/prove03/main.js
+++ web/prove03/main.js
@@ -1,23 +1,18 @@
 function addToSession(event) {
     // Get the src of the image
+    var xhttp = new XMLHttpRequest();
     let name = event.target.name;
     let value = event.target.value;
 
     console.log(name);
     console.log(value);
-    // Send Ajax request to backend.php, with src set as "img" in the POST data
-    $.post("add.php", {name: value});
+    
+    xhttp.open("POST", "add.php", true);
+  xhttp.setRequestHeader(name, value);
+  xhttp.send();
 }
 
 var formInputs = document.getElementsByClassName("unit-input");
 for (var i = 0; i < formInputs.length; i++) {
     formInputs[i].addEventListener("click", (event) => addToSession(event));
 }
-
-$(document).ready(function(){
-    $("unit-input").click(function(){
-        $.post("add.php", function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
-        });
-    });
-});
