95392f0fb46eec6e76ec480ee6bcb4bdc4747b0d
Kevin <rocketlikeabass@gmail.com>
2018-05-10 22:26:09 -0600

    tracking values

--- web/prove03/viewcart.php
+++ web/prove03/viewcart.php
@@ -1,16 +1,10 @@
 <?php
-  session_start();
-  if (isset($_SESSION["count"])) {
-      $_SESSION["count"] += 1;
-  }
-  else {
-      $_SESSION["count"] = 0;
-  }
+  session_start()
 ?>
 <html>
   <body>
     <?php
-      echo "<h1>" . $_SESSION["count"] . "</h1>";
+      echo "<h1>" . $_SESSION["DarkTemplar"] . "</h1>";
       //echo "<p>viewcart</p>"
     ?>
   </body>
