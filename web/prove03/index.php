fb0eeda58e3d00ecd3498bdea05c0968bdcbe5af
Kevin Bunn <rocketlikeabass@gmail.com>
2018-05-11 00:19:14 -0600

    session values

--- web/prove03/index.php
+++ web/prove03/index.php
@@ -1,10 +1,10 @@
 <?php
    session_start();
-   $_SESSION["DarkTemplar"] = '0';
-   $_SESSION["Stalker"] = '0';
-   $_SESSION["Colossus"] = '0';
-   $_SESSION["Pheonix"] = '0';
-   $_SESSION["Artanis"] = '0';
-   $_SESSION['units'] = '0';
+   $_SESSION["DarkTemplar"] = 0;
+   $_SESSION["Stalker"] = 0;
+   $_SESSION["Colossus"] = 0;
+   $_SESSION["Pheonix"] = 0;
+   $_SESSION["Artanis"] = 0;
+   $_SESSION['units'] = '';
    header('Location: browse.php');
 ?>
