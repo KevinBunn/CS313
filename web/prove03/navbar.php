e2c2dcb55f35c7c4f16297f17af34f949cdef813
Kevin <rocketlikeabass@gmail.com>
2018-05-11 14:14:03 -0600

    title

--- web/prove03/navbar.php
+++ web/prove03/navbar.php
@@ -1,13 +1,13 @@
         <!-- Bootstrap Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">Spear of Adun Warp Tech</a>
+        <a class="navbar-brand" style="color:white" href="#">Spear of Adun Warp Tech</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+          <ul class="navbar-nav mr-auto" style="color:white">
             <?php if($_SERVER["PHP_SELF"]=="browse.php"){echo '<li class="nav-item active">';} else {echo '<li class="nav-item">';} ?>
               <a class="nav-link" href="browse.php">Select Units <span class="sr-only">(current)</span></a>
             </li>
