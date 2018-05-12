c4f0d4500998bb43b1e54e8f0caed9d67b766696
Kevin <rocketlikeabass@gmail.com>
2018-05-12 17:29:40 -0600

    confirmation

--- web/prove03/checkout.php
+++ web/prove03/checkout.php
@@ -36,6 +36,10 @@
       </div>      
     </div>    
     
+    <?php
+    include "redirect.php";  
+    ?>  
+      
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous">
     </script>
     <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js">
