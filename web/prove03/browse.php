0d66c684ee8c8676dbe8a0b361f36b9744049f48
Kevin <rocketlikeabass@gmail.com>
2018-05-11 18:28:02 -0600

    view cart

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -29,8 +29,7 @@
             </div>
         </div>
       </div>
-  </div>
-  <div id="main">    
+  </div>   
       <div id="container">
         <div class="row justify-content-center">
           <div class="col-lg-6">
@@ -79,7 +78,6 @@
           </div>
         </div>
     </form> 
-  </div>   
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous">
     </script>
     <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js">
