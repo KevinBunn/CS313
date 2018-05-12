9424ef0d39fe298019153a70ddd3bc81bbaf153a
Kevin <rocketlikeabass@gmail.com>
2018-05-12 17:23:48 -0600

    confirmation

--- web/prove03/redirect.php
+++ web/prove03/redirect.php
@@ -1,10 +1,9 @@
 <?php
-if (isset($_POST['submit'])) {
-    $_SESSION["Address"] = test_input($_POST["Address"]);
-    $_SESSION["City"] = test_input($_POST["City"]);
-    $_SESSION["State"] = test_input($_POST["State"]);
-    $_SESSION["Zip"] = test_input($_POST["Zip"]);
-} 
+
+$_SESSION["Address"] = test_input($_POST["Address"]);
+$_SESSION["City"] = test_input($_POST["City"]);
+$_SESSION["State"] = test_input($_POST["State"]);
+$_SESSION["Zip"] = test_input($_POST["Zip"]); 
 
 function test_input($data) {
     $data = trim($data);
