a136fb875b3f357137b3ba71031c221d4aa1e3fc
Kevin <rocketlikeabass@gmail.com>
2018-05-12 18:27:40 -0600

    confirmation

--- web/prove03/confirmation.php
+++ web/prove03/confirmation.php
@@ -22,37 +22,57 @@
     </div>
       </div>  
         <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-6">
-              <div id="content">
-                <div class="unit-display" id="content-dt">    
-                  <img src="resources/dark_templar.png" alt="Dark Templar">
-                  <?php echo "<h1>" . $_SESSION["DarkTemplar"] . "</h1>"?>
-                  <br>
-                </div>
+          <div class="row justify-content-between">
+            <div class="col-lg-4">
+              <div class="unit-display" id="content-dt">    
+                <img src="resources/dark_templar.png" alt="Dark Templar">
+                <?php echo "<h1>" . $_SESSION["DarkTemplar"] . "</h1>"?>
+            </div>  
+          </div>
+          <div class="col-lg-4">
+            <div class="unit-display" id="content-dt">    
+                <img src="resources/zergling.png" alt="Dark Templar">
+                <h1>22</h1>
+            </div>  
+          </div>
+        </div>
+          <div class="row">
+              <div class="col-lg-4">
                 <div class="unit-display" id="content-st">    
                   <img src="resources/stalker.png" alt="Stalker">
                   <?php echo "<h1>" . $_SESSION["Stalker"] . "</h1>"?>
                 </div>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-lg-4">
                 <div class="unit-display" id="content-col">
                   <img src="resources/colossus.png" alt="Colossus">
                   <?php echo "<h1>" . $_SESSION["Colossus"] . "</h1>"?>
                 </div>
+              </div>    
+          </div>
+          <div class="row">
+              <div class="col-lg-4">
                 <div class="unit-display" id="content-ph">  
                   <img src="resources/pheonix.png" alt="Pheonix">
                   <?php echo "<h1>" . $_SESSION["Pheonix"] . "</h1>"?>
                 </div>
-                  <?php
+              </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-4">
+                <?php
                     if ($_SESSION["Artanis"] == 1) {
                      echo "<div class=\"unit-display\" id=\"content-ar\">
                       <img src=\"resources/artanis.png\" alt=\"Artanis\">
                     </div>";
                     }
                   ?>
-              </div>
-            </div>
+            </div>    
           </div>
-        </div> 
+        </div>
+            
     
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous">
     </script>
