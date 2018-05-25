8c94676a3117effe7460f121a21a009f946cb0c2
Kevin <rocketlikeabass@gmail.com>
2018-05-25 12:33:17 -0600

    project names

--- web/workflow/dashboard.php
+++ web/workflow/dashboard.php
@@ -32,7 +32,7 @@
     //var_dump($projects);
     
     foreach ($projects as $project) {
-      echo '<div><a href="project.php?project=' . $project["project_id"] . '">' . $project["name"] . "</a></div>";
+      echo '<a href="project.php?project=' . $project["project_id"] . '">' . $project["name"] . "</a>";
     }
     
   ?>
