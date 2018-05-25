08a4c4e15af40f36e4d03eb7ccb1933cf1dff4b7
Kevin Bunn <rocketlikeabass@gmail.com>
2018-05-24 21:21:19 -0600

    login

--- web/workflow/login.php
+++ web/workflow/login.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-
+    
 ?>
 
 <html>
@@ -9,10 +9,40 @@
     </head>
 <body>
     <h1>Login</h1>
-    <form>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
         Username: <input type="text" name="username">
         Password: <input type="text" name="password">
         <input type="submit">
     </form>
+    <?php  
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  
+    $stmt = $db->prepare('SELECT username, password FROM user');
+    $stmt->execute();
+    $rows = $stmt->fetchALL(PDO::FETCH_ASSOC);
+  
+    if (isset($_POST["username"]) && isset($_POST["password"])) {
+      $successful_login = FALSE;
+      foreach ($rows as $row) {
+        if ($row["username"] == $_POST["username"] && $row["password"] == $_POST["password"])
+          $successful_login = TRUE;
+      }
+      if($successful_login){
+        header('Location: dashboard.php');
+      }
+      else
+        echo "<p style=\"text-color: red\">Username or Password is incorrect</p>";
+    }
+  ?>
 </body>
 </html>
