5645122add0f37e782d6a5e58bfba4f7d10b7be5
Kevin <rocketlikeabass@gmail.com>
2018-06-01 17:13:13 -0600

    dbConnect

--- web/workflow/login.php
+++ web/workflow/login.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-    require_once("dbConnect.php");
+    require("/app/web/workflow/dbConnect.php");
     $db = get_db();
     if (!isset($db)) {
 	   die("DB Connection was not set");
