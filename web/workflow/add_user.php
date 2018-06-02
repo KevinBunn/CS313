b06fcbc339f524f5a8d2e48c109d1dd3e0693491
Kevin <rocketlikeabass@gmail.com>
2018-06-01 18:30:08 -0600

    add user

--- web/workflow/add_user.php
+++ web/workflow/add_user.php
@@ -13,13 +13,12 @@ $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 $passwordConfirm = filter_input(INPUT_POST, 'password-confirm', FILTER_SANITIZE_STRING);
 
-
 if(strcmp($password,$passwordConfirm) !== 0) {
     $_SESSION['signup_error'] = "passwords do not match";
     header('Location: signup.php');
 }
 
-function insertNewUser($firstName, $lastName, $username, $password) {
+function insertNewUser($firstName, $lastName, $username, $password, $db) {
     $stmt = $db->prepare("INSERT INTO \"user\" (first_name, last_name, username, password, date_joined) VALUES(:firstname, :lastname, :username, :password, datejoined)");
     $stmt->bindValue(':firstname', $firstName, PDO::PARAM_STR);
     $stmt->bindValue(':lastname', $lastName, PDO::PARAM_STR);
@@ -32,7 +31,7 @@ function insertNewUser($firstName, $lastName, $username, $password) {
     return $rowsChanged;
 }
 
-$rowsAffected = insertNewUser($firstName, $lastName, $username, $password);
+$rowsAffected = insertNewUser($firstName, $lastName, $username, $password, $db);
 if ($rowsAffected == 0) {
     $_SESSION['signup_error'] = "Nothing was inserted";
     header('Location: signup.php');
