9baaffdbd4d3c6804360b8f644b054675387f09a
Kevin <rocketlikeabass@gmail.com>
2018-06-02 20:55:00 -0600

    add new project

--- web/workflow/dashboard.js
+++ web/workflow/dashboard.js
@@ -12,5 +12,5 @@ addProject.addEventListener("click", function(event) {
     newForm.appendChild(newTextInput);
     newForm.appendChild(newSubmit);
     event.target.parentElement.appendChild(newForm);
-    event.target.parentElement.removeChild(event.target.querySelector('a'));
+    event.target.parentNode.removeChild(event.target.querySelector('a'));
 });
