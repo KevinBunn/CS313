a01aa1929cbf3aaa02f1786825b6064ce1cdcc25
Kevin <rocketlikeabass@gmail.com>
2018-06-02 20:22:33 -0600

    add new project

--- web/workflow/dashboard.php
+++ web/workflow/dashboard.php
@@ -36,7 +36,7 @@
       echo '<div class="category"><a href="project.php?project=' . $project["project_id"] . '">' . $project["name"] . '</a></div>';
     }
     
-    echo '<div id="add-project" class="category"><a class="add-project-link">create new project...</a></div>';
+    echo '<div id="add-project" class="category"><a class="add-project-link" href="#">create new project...</a></div>';
     
   ?>
     </div>
