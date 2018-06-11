72d972d53a87003ab95d46d7879e8040d12c4776
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-11 14:51:35 -0600

    task tables

--- web/workflow/projectView.js
+++ web/workflow/projectView.js
@@ -17,3 +17,22 @@ addCategory.addEventListener("click", function(event) {
     event.target.parentElement.parentElement.appendChild(newForm);
     event.target.parentElement.parentElement.removeChild(event.target.parentElement);
 });
+
+var statusFields = document.getElementsByClassName("task-status");
+
+for (statusField of statusFields) {
+  let status = statusField.innerHTML;
+  switch (status) {
+    case "Open":
+        statusField.backgroundColor = "#FF6E00";
+        break;
+    case "Test":
+        statusField.backgroundColor = "#F8D801";
+        break;
+    case "Complete":
+        statusField.backgroundColor = "#45E629";
+        break;
+    default:
+        console.log("you're all noobs");
+  }
+}
