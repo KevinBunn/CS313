9fa5b3bf3619fac80b6105608080457f90786e13
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-11 22:46:43 -0600

    login

--- web/workflow/signup.php
+++ web/workflow/signup.php
@@ -15,7 +15,7 @@
         <input class="custom-input" type="text" name="last-name" placeholder="Last Name"><br>
         <input class="custom-input" type="text" name="username" placeholder="Username"><br>
         <input class="custom-input" type="password" name="password" placeholder="Password"><br>
-        <input class="custom-input" type="password" name="password-confirm" placeholder="Password Confirm">
+        <input class="custom-input" type="password" name="password-confirm" placeholder="Password Confirm"><br>
         <input class="front-page-button" type="submit">
     </form>
     <?php
