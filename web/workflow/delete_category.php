0127dc1706a306825a454026d0e94704f38abd42
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-12 10:55:08 -0600

    delete_category

--- web/workflow/delete_category.php
+++ web/workflow/delete_category.php
@@ -11,11 +11,11 @@
     $projectId = $_GET['project'];
 
     $deleteTaskStmt = $db->prepare("DELETE FROM task WHERE category_id = :category_id");
-    $deleteTaskStmt->bindValue(':category_id', categoryId, PDO::PARAM_INT);
+    $deleteTaskStmt->bindValue(':category_id', $categoryId, PDO::PARAM_INT);
     $deleteTaskStmt->execute();
     
     $deleteCategoryStmt = $db->prepare("DELETE FROM category WHERE category_id = :category_id");
-    $deleteCategoryStmt->bindValue(':category_id', categoryId, PDO::PARAM_INT);
+    $deleteCategoryStmt->bindValue(':category_id', $categoryId, PDO::PARAM_INT);
     $deleteCategoryStmt->execute();
 
     header('Location: project.php?project=' . $projectId);
