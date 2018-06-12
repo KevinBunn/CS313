7f92226b57bbdff339615b016dd6a7bca29218be
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-12 09:52:26 -0600

    delete_project

--- web/workflow/delete_project.php
+++ web/workflow/delete_project.php
@@ -6,4 +6,34 @@
     if (!isset($db)) {
         die("DB Connection was not set");
     }
+
+    $projectId = $_GET['project'];
+
+    $getCategotyQuery = "SELECT category_id FROM category WHERE project_id = :project_id";
+    $getCategoriesStmt = $db->prepare($getCategoryQuery);
+    $getCategoriesStmt->bindValue(':project_id', $projectId, PDO::PARAM_INT);
+    $categories = $getCategoriesStmt->execute();
+
+    foreach ($categories as $category) {
+      $categoryId = $category["category_id"];
+      
+      $deleteTaskStmt = $db->prepare("DELETE FROM task WHERE category_id = :category_id");
+      $deleteTaskStmt->bindValue(':category_id', categoryId, PDO::PARAM_INT);
+      $deleteTaskStmt->execute();
+    }
+
+    $deleteCategoryStmt = $db->prepare("DELETE FROM category WHERE project_id = :project_id");
+    $deleteCategoryStmt->bindValue(':project_id', $projectId, PDO::PARAM_INT);
+    $deleteCategoryStmt->execute();
+
+    deleteProjectStmt = $db->prepare("DELETE FROM project WHERE project_id = :project_id");
+    $deleteProjectStmt->bindValue(':project_id', $projectId, PDO::PARAM_INT);
+    deleteProjectStmt->execute();
+
+    deleteUserToProjectStmt = $db->prepare("DELETE FROM user_to_project WHERE project_id = :project_id");
+    deleteUserToProjectStmt->bindValue(':project_id', $projectId, PDO::PARAM_INT);
+    deleteUserToProjectStmt->execute();
+
+    header('Location: dashboard.php');
+  
 ?>
