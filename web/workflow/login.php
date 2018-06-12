48e6978c133ec3d88c77359c3a229cc68c4d372a
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-11 22:59:07 -0600

    login

--- web/workflow/login.php
+++ web/workflow/login.php
@@ -34,7 +34,7 @@
     if (isset($_POST["username"]) && isset($_POST["password"])) {
       $successful_login = FALSE;
       foreach ($rows as $row) {
-        if ($rows[0]["username"] == $_POST["username"] && password_verify($rows[0]["password"], $_POST["password"])) {
+        if ($row["username"] == $_POST["username"] && password_verify($row["password"], $_POST["password"])) {
             $successful_login = TRUE;
         }
       }
